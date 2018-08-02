99c2af3f968d9284b0075a21866d31793d6bcdb0
Amarell <burhanudinwakhid@student.uns.ac.id>
2018-08-02 22:15:07 +0700

    baru

--- app/src/main/java/com/dark/amarel/temanbelajar/daftar_aktivity.java
+++ app/src/main/java/com/dark/amarel/temanbelajar/daftar_aktivity.java
@@ -1,13 +1,25 @@
 package com.dark.amarel.temanbelajar;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class daftar_aktivity extends AppCompatActivity {
-
+    private ImageView back_logo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_daftar_aktivity);
+
+        back_logo=findViewById(R.id.back_logo);
+        back_logo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(daftar_aktivity.this, login_murid.class);
+                startActivity(intent); }
+        });
+
     }
 }
--- app/src/main/java/com/dark/amarel/temanbelajar/form_profil.java
+++ app/src/main/java/com/dark/amarel/temanbelajar/form_profil.java
@@ -1,11 +1,15 @@
 package com.dark.amarel.temanbelajar;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.Spinner;
 
 public class form_profil extends AppCompatActivity {
+private ImageView back_logo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,13 +17,18 @@ public class form_profil extends AppCompatActivity {
         setContentView(R.layout.activity_form_profil);
 
     Spinner spinner = (Spinner) findViewById(R.id.spinner) ;
-
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.jenjang, android.R.layout.simple_spinner_item);
-
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-
         spinner.setAdapter(adapter);
 
+        back_logo=findViewById(R.id.back_logo);
+        back_logo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(form_profil.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
