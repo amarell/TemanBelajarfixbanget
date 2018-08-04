10926593cca9be6dfb8f175c994b39cbcd880517
amarell <burhanudinwakhid@student.uns.ac.id>
2018-08-04 18:54:24 +0700

    burhan

--- app/src/main/java/com/dark/amarel/temanbelajar/daftar_aktivity.java
+++ app/src/main/java/com/dark/amarel/temanbelajar/daftar_aktivity.java
@@ -4,14 +4,45 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class daftar_aktivity extends AppCompatActivity {
     private ImageView back_logo;
+    private EditText nama, email, hp, password, c_password;
+    private Button btn_regist;
+
+    private ProgressBar loading;
+    private static String URL_REGIST = "https://buburwakhid.000webhostapp.com/magang/register.php";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_daftar_aktivity);
+
+        loading=findViewById(R.id.loading);
+        nama=findViewById(R.id.Et_nama_regis);
+        email=findViewById(R.id.Et_email_regis);
+        hp=findViewById(R.id.Et_hp_regis);
+        password=findViewById(R.id.Et_pass_regis);
+        c_password=findViewById(R.id.Et_passcon_regis);
+        btn_regist=(Button) findViewById(R.id.btn_daftar_regis);
 
         back_logo=findViewById(R.id.back_logo);
         back_logo.setOnClickListener(new View.OnClickListener() {
@@ -20,6 +51,67 @@ public class daftar_aktivity extends AppCompatActivity {
                 Intent intent = new Intent(daftar_aktivity.this, login_murid.class);
                 startActivity(intent); }
         });
+
+        btn_regist.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Regist();
+            }
+        });
+
+    }
+    private void Regist(){
+        loading.setVisibility(View.VISIBLE);
+        btn_regist.setVisibility(View.GONE);
+
+        final String nama = this.nama.getText().toString().trim();
+        final String email = this.email.getText().toString().trim();
+        final String hp = this.hp.getText().toString().trim();
+        final String password = this.password.getText().toString().trim();
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL_REGIST,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try{
+                            JSONObject jsonObject = new JSONObject(response);
+                            String success = jsonObject.getString("success");
+
+                            if(success.equals("1")){
+                                Toast.makeText(daftar_aktivity.this, "Register berhasil", Toast.LENGTH_SHORT).show();
+                            }
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            Toast.makeText(daftar_aktivity.this, "Register gagal" + e.toString(), Toast.LENGTH_SHORT).show();
+                            loading.setVisibility(View.GONE);
+                            btn_regist.setVisibility(View.VISIBLE);
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(daftar_aktivity.this, "Register gagal" + error.toString(), Toast.LENGTH_SHORT).show();
+                        loading.setVisibility(View.GONE);
+                        btn_regist.setVisibility(View.VISIBLE);
+                    }
+                })
+
+        {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                params.put("nama", nama);
+                params.put("email", email);
+                params.put("password", password);
+                params.put("hp", hp);
+                return params;
+            }
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
 
     }
 }
