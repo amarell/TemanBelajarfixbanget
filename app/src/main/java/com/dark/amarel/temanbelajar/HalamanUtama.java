f7813be9df40fc7e9d301f41e9cdecf0217412bc
Amarell <burhanudinwakhid@student.uns.ac.id>
2018-08-14 14:54:38 +0700

    dashboard

--- app/src/main/java/com/dark/amarel/temanbelajar/HalamanUtama.java
+++ app/src/main/java/com/dark/amarel/temanbelajar/HalamanUtama.java
@@ -1,5 +1,6 @@
 package com.dark.amarel.temanbelajar;
 
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -80,17 +81,17 @@ public class HalamanUtama extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.nav_beranda) {
+            android.support.v4.app.FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.replace(R.id.flberanda,new BerandaFragment());
+            ft.commit();
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_kategori_les) {
 
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_profil) {
+            android.support.v4.app.FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.replace(R.id.flprofil,new ProfilFragment());
+            ft.commit();
 
         }
 
