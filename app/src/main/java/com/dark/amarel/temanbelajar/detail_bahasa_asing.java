36610b15b0784661f4f229c5c64b3cdc1f38ed2d
Amarell <burhanudinwakhid@student.uns.ac.id>
2018-08-16 15:11:48 +0700

    Anggit

--- app/src/main/java/com/dark/amarel/temanbelajar/detail_bahasa_asing.java
+++ app/src/main/java/com/dark/amarel/temanbelajar/detail_bahasa_asing.java
@@ -1,13 +1,24 @@
 package com.dark.amarel.temanbelajar;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class detail_bahasa_asing extends AppCompatActivity {
-
+    private ImageView back_logo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_bahasa_asing);
+
+        back_logo=findViewById(R.id.back_logo_bahasa_asing);
+        back_logo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(detail_bahasa_asing.this, Dashboard.class);
+                startActivity(intent); }
+        });
     }
 }
--- app/src/main/java/com/dark/amarel/temanbelajar/detail_keterampilan.java
+++ app/src/main/java/com/dark/amarel/temanbelajar/detail_keterampilan.java
@@ -1,13 +1,24 @@
 package com.dark.amarel.temanbelajar;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class detail_keterampilan extends AppCompatActivity {
-
+    private ImageView back_logo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_keterampilan);
+
+        back_logo=findViewById(R.id.back_logo_keterampilan);
+        back_logo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(detail_keterampilan.this, Dashboard.class);
+                startActivity(intent); }
+        });
     }
 }
