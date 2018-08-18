b4e295f07a3d5733826c326bf981b2eab7818891
Amarell <burhanudinwakhid@student.uns.ac.id>
2018-08-18 15:27:31 +0700

    re

---
+++ app/src/main/java/com/dark/amarel/temanbelajar/detail_smp.java
@@ -0,0 +1,26 @@
+package com.dark.amarel.temanbelajar;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.View;
+import android.widget.ImageView;
+
+public class detail_smp extends AppCompatActivity {
+    private ImageView back_logo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_smp);
+
+        back_logo=findViewById(R.id.back_logo_smp);
+        back_logo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(detail_smp.this, detail_materi_sekolah.class);
+                startActivity(intent); }
+        });
+    }
+}
