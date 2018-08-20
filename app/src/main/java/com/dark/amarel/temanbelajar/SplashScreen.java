ed0e421410a6ed0078c53643a83556579c69dc4b
Amarell <burhanudinwakhid@student.uns.ac.id>
2018-08-20 14:42:15 +0700

    anggit

---
+++ app/src/main/java/com/dark/amarel/temanbelajar/SplashScreen.java
@@ -0,0 +1,39 @@
+package com.dark.amarel.temanbelajar;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        ImageView temanbelajar_splash= findViewById(R.id.temanbelajar_splash);
+
+        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.fade);
+        temanbelajar_splash.startAnimation(animation);
+
+        Thread timer = new Thread(){
+            @Override
+            public void run() {
+
+                try {
+                    sleep(3000);
+                    Intent intent=new Intent(getApplicationContext(), login_murid.class);
+                    startActivity(intent);
+                    finish();
+                    super.run();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        timer.start();
+    }
+}
