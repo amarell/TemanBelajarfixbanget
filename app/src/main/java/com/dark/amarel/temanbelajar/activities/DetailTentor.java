260110c73088fd7cb97fdd2b72063dedb69b06c4
Amarell <burhanudinwakhid@student.uns.ac.id>
2018-08-20 14:34:37 -0700

    bubur

---
+++ app/src/main/java/com/dark/amarel/temanbelajar/activities/DetailTentor.java
@@ -0,0 +1,13 @@
+package com.dark.amarel.temanbelajar.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.dark.amarel.temanbelajar.R;
+public class DetailTentor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_tentor);
+    }
+}
